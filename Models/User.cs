d402a9cbe047cfd8f9fbb12f9b4f3dab7ebfa18b
Justin Holsclaw <justin.holsclaw@ad.snow.edu>
2020-08-25 14:38:36 -0600

    first commit

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace hello_world.Models
+{
+    public class User
+    {
+        public string Username{get; set; }
+
+        public string Fullname {get; set; }
+
+        public string Password {get; set; }
+    }
+}
